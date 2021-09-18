ee25e5f3d4ed56ad36c6382581ee9c4f8d8217ae
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-09-18 10:04:47 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="vh-100"></main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+body {
+  background: url(/jpegs/Mozart-Lange.jpg) no-repeat center center;
+  background-size: cover;
+  overflow: hidden;
+}
+</style>
