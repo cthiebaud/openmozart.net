0953e06a79d6ad20beb4c2ebfab772580e48aa79
Christophe Thiebaud <christophe.thiebaud@alumni.insead.edu>
2021-09-26 17:39:55 +0200

    detricotons !

--- detricote.sh
+++ detricote.sh
@@ -2,18 +2,19 @@
 set -e
 shopt -s nullglob
 ignore() {
-  	git reset HEAD $1
-	git checkout -- $1
+   git reset HEAD $1
+   git checkout -- $1
 }
 for fname in static/favicon/*; do
    ignore ${fname}
 done
+
 List=(
-	  README.md
-	  package.json
-      static/git.txt 
-      static/CNAME 
-     )
+   README.md
+   package.json
+   static/git.txt 
+   static/CNAME 
+)
 for fname2 in ${List[*]}; do
    ignore ${fname2}
 done 
